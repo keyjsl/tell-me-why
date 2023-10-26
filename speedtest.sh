1dcf7069ab0555d7a92c17c93d7b39d5c15df54d
keyjsl <111936411+keyjsl@users.noreply.github.com>
2023-10-26 18:26:07 +0800

    Update and rename check_network.sh to speedtest.sh

---
+++ speedtest.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Run librespeed-cli and capture the result
+speedtest_result=$(librespeed-cli)
+
+# Your Telegram bot token and chat ID
+TELEGRAM_BOT_TOKEN="6269466007:AAEqvCFRZ9KuHYn7MuP3PEghRG_m3aKrKiQ"
+TELEGRAM_CHAT_ID="635970015"
+
+# Send the result to Telegram using curl
+curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage" -d "chat_id=$TELEGRAM_CHAT_ID" -d "text=$speedtest_result"
