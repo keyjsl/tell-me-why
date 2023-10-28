b934a7adfe4dbfd07f984d21a425839bb81cae36
keyjsl <111936411+keyjsl@users.noreply.github.com>
2023-10-29 00:29:37 +0800

    Update speedtest.sh

--- speedtest.sh
+++ speedtest.sh
@@ -4,8 +4,8 @@
 speedtest_result=$(librespeed-cli)
 
 # Your Telegram bot token and chat ID
-TELEGRAM_BOT_TOKEN="6269466007:AAEqvCFRZ9KuHYn7MuP3PEghRG_m3aKrKiQ"
-TELEGRAM_CHAT_ID="635970015"
+TELEGRAM_BOT_TOKEN="token here"
+TELEGRAM_CHAT_ID="id here"
 
 # Send the result to Telegram using curl
 curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage" -d "chat_id=$TELEGRAM_CHAT_ID" -d "text=$speedtest_result"
